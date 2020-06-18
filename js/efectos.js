1afc512848e96bb129d57ac558a88afdebbfa873
Daniel SA <d.santamaria@uniandes.edu.co>
2020-06-18 15:16:09 -0500

    efectos y boton mk

--- js/efectos.js
+++ js/efectos.js
@@ -3,3 +3,21 @@ $(".menu-toggle-btn").click(function(){
     $(this).toggleClass("fa-times");
     $(".navigation-menu").toggleClass("active");
 });
+
+
+//boton arriba
+$('.ir-arriba').click(function(){
+    $('body, html').animate({
+        scrollTop:'0px'
+    }, 300);
+});
+
+$(window).scroll(function(){
+    if($(this).scrollTop()>0){
+        $('.ir-arriba').slideDown(300);
+    }else{
+        $('.ir-arriba').slideUp(300);
+    }
+});
+
+
