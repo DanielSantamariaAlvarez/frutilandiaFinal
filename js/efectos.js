f4b9005d316ed94b560f83abc42cd1f368764ee1
Daniel SA <d.santamaria@uniandes.edu.co>
2020-06-18 11:23:53 -0500

    avance mucho

---
+++ js/efectos.js
@@ -0,0 +1,5 @@
+//Menú desplegable-------------------------------
+$(".menu-toggle-btn").click(function(){
+    $(this).toggleClass("fa-times");
+    $(".navigation-menu").toggleClass("active");
+});
