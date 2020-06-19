2af5278caeb555d2f5462598987dd2a887865617
Daniel SA <d.santamaria@uniandes.edu.co>
2020-06-19 16:46:25 -0500

    Prueba de carousel

--- js/efectos.js
+++ js/efectos.js
@@ -5,7 +5,7 @@ $(".menu-toggle-btn").click(function(){
 });
 
 
-//boton arriba
+//boton arriba -----------------------------------
 $('.ir-arriba').click(function(){
     $('body, html').animate({
         scrollTop:'0px'
@@ -18,6 +18,48 @@ $(window).scroll(function(){
     }else{
         $('.ir-arriba').slideUp(300);
     }
+});
+
+
+//Carousel de fotos -------------------------------
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carouselLista'), {
+        draggable: true,
+        rewind:true,
+        dots: '.carouselIndicadores',
+        autoplay: 2000,
+        arrows: {
+            prev: '.carouselAnterior',
+            next: '.carouselSiguiente'
+        },
+        responsive: [
+            {
+                // screens greater than >= 800px
+                breakpoint: 300,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                // screens greater than >= 800px
+                breakpoint: 450,
+                settings: {
+                    // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },{
+                // screens greater than >= 1024px
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
 
 
